1b7db76adc314cfd245bcc7decc89f1c61855f88
Hannah Weeks <hannahlweeks@gmail.com>
2017-08-10 14:50:03 -0500

    fixed issue when times=NULL

--- R/predict.pkm.R
+++ R/predict.pkm.R
@@ -9,7 +9,7 @@
 #' @examples
 #'
 
-predict.pkm <- function(object, times){
+predict.pkm <- function(object, times = NULL){
 
   # Parameter estimates from fitted pkm model
   est <- object$est
@@ -17,12 +17,16 @@ predict.pkm <- function(object, times){
 
 
   ## Posterior estiamte
-  con <- apply(object$sol.eqn(times)*1000, 2, function(x) pmax(0,x))
+  if(is.null(times)){
+    return(data.frame("Time" = object$fitted.values$time,
+                      "Concentration" = object$fitted.values$conc))
+  }else{
+    con <- apply(object$sol.eqn(times)*1000, 2, function(x) pmax(0,x))
+    return(data.frame("Time" = times,
+                      "Concentration" = con))
+  }
 
   # # Predicted ft>mic estimate
   # ftmic <- mic_stat(pars = est$par, newivt, object$data,
   #                   tms, con[1,], th = object$thresh)
-
-
-  return(data.frame("conc" = con, "tms" = times))
 }
